7649d92f46022c5910b8b2d17b91ab6154dafdbb
RalfVogel <ralf@ravo-it.de>
2022-03-09 20:06:46 +0100

    cards

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -27,6 +27,21 @@ const url = require('url');
 // console.log("Will read file!");
 
 //////////////Server
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8'
@@ -49,7 +64,12 @@ const server = http.createServer((req, res) => {
   // Overview Page
   if (pathName === '/' || pathName === '/overview') {
     res.writeHead(200, { 'Content-type': 'text/html' });
-    res.end(tempOverview);
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+    res.end(output);
 
     // Product Page
   } else if (pathName === '/product') {
